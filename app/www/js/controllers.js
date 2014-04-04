c78c03ee47735c4f5ccfd985d2e79db42fac29f6
ziaxdk <ziaxdk@gmail.com>
2014-04-04 14:45:31 +0200

    aktuel

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -12,15 +12,36 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('CurrentCtrl', function($scope, $ionicGesture) {
-    $scope.items = [{ count: 2, text: 'l mælk' }, { count: 6, text: 'stk æg' }];
-    console.log($ionicGesture);
+.controller('CurrentCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('current-new-line.html', {
+    scope: $scope,
+    focusFirstInput: true,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modalNewLine = modal;
+  });
 
-    $scope.tap = function() {
-      console.log('tap');
-    }
+  $scope.newline = {};
+  $scope.items = [{id: '1', count: 2, unit: 'l', text: 'mælk' }, { id: 2, count: 6, unit: 'stk', text: 'æg' }];
 
-  })
+  $scope.tap = function(line) {
+    // $scope.items.remove(line);
+  };
+
+  $scope.clearNewLine = function(line) {
+    $scope.newline = {};
+  };
+
+  $scope.createNewLine = function(line) {
+    $scope.items.push(angular.copy(line));
+    $scope.newline = {};
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modalNewLine.remove();
+  });
+
+})
 
 .controller('AppCtrl', function($scope) {
 });
